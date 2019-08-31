cc53e544209588d69c601d08a5a4767be17dcc3d
Nilesh Jain <nileshjain166@gmail.com>
2019-08-31 12:38:53 +0530

    added cart icon and dropdown and implemented toggleCartHidden using cart reducer and action

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.util'
 import { ReactComponent as Logo } from '../../assets/crown.svg'
 import { connect } from 'react-redux'
+
+import CartIcon from '../cart-icon/cart-icon.component'
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.styles.scss'
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
     return (
         <div className='header'>
             <Link className='logo-container' to='/'>
@@ -29,13 +31,20 @@ const Header = ({ currentUser }) => {
                             SIGN IN
                         </Link>
                 }
+                <CartIcon />
             </div>
+            {
+                hidden ?
+                    null :
+                    <CartDropdown />
+            }
         </div>
     )
 }
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
